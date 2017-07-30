4dbdecea20157ef55101baad4deaf7495a09cea9
danielmorelo <jmorelo@lasillavacia.com>
2017-07-30 12:21:21 -0500

    cambio del mapa en la imagen

--- src/base.js
+++ src/base.js
@@ -43,7 +43,7 @@ export default class Base extends Component {
         {popup}
 
         <div className="cont_circulos">
-          <img src="https://c1.staticflickr.com/5/4304/36226500955_60dd73b194_b.jpg" alt=""
+          <img src="https://c1.staticflickr.com/5/4302/35463088463_986f6ba815_b.jpg" alt=""
                className="background-img"/>
 
           <div onClick={this.handonclic.bind(this, "Los hemanos Daes, megacontratistas de Barranquilla y dueños de Tecnoglass", "<p>En enero de 2010, durante su primera administración como alcalde de Barranquilla, Alejandro Char firmó un otrosí al contrato de concesión de amoblamiento urbano que desde 1999 maneja la empresa Construseñales SA, de los hermanos Christian y José Manuel ‘Yuyo’ Daes Abuchaibe, superpoderosos de Barranquilla y dueños de la empresa Tecnoglass. Con ese otrosí, el Distrito le entregó a un privado el 35% del total del recaudo de las fotomultas. Desde entonces, la Alcaldía instala cámaras por toda la ciudad, que hoy suman 56, para registrar infracciones por exceso de velocidad, irrespeto a las cebras y semáforos, bloqueo de intersecciones o incumplimiento a la medida de 'pico y placa'.</p><p> Entre 2010 y febrero de este año, la Secretaría de Tránsito impuso 744 mil comparendos electrónicos por 187 mil millones de pesos. Sin embargo, solo recaudó 30.663 millones de pesos porque los 157.202 millones restantes están en cobros coactivos, según respuesta de esa dependencia a un derecho de petición. Desde su instalación, las cámaras han generado polémica en Barranquilla porque, pese a que la Alcaldía las presentó como aliadas en su campaña para reducir la accidentalidad vial, son consideradas elementos de generación de recursos que no siempre se instalan con criterios técnicos.</p>" +
